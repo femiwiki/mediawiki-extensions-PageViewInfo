aa331b88f80f6b9aa6be63431bb0328ba0b2d9cd
lens0021 <lorentz0021@gmail.com>
2021-05-30 13:47:38 +0900

    Introduce a new hook for replacing WikimediaPageView Service with subclass of PageViewService
    Gives a chance to use other PageViewService than
    WikimediaPageViewService which is strongly limited to the demand of WMF,
    to other extensions.
    
    Bug: T283779
    Change-Id: Iee91eda8378b82dbdb9b2ad287a5b209c83705e4

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -11,17 +11,21 @@ return [
 	'PageViewService' => function ( MediaWikiServices $services ) {
 		$mainConfig = $services->getMainConfig();
 		$extensionConfig = $services->getConfigFactory()->makeConfig( 'PageViewInfo' );
-		$endpoint = $extensionConfig->get( 'PageViewInfoWikimediaEndpoint' );
-		$project = $extensionConfig->get( 'PageViewInfoWikimediaDomain' )
-			?: $mainConfig->get( 'ServerName' );
 		$cache = ObjectCache::getLocalClusterInstance();
 		$logger = LoggerFactory::getInstance( 'PageViewInfo' );
 		$cachedDays = max( 30, $extensionConfig->get( 'PageViewApiMaxDays' ) );
 
-		$service = new WikimediaPageViewService( $endpoint, [ 'project' => $project ],
-			$extensionConfig->get( 'PageViewInfoWikimediaRequestLimit' ) );
-		$service->setLogger( $logger );
-		$service->setOriginalRequest( RequestContext::getMain()->getRequest() );
+		$service = \Hooks::run( 'PageViewInfoGetPageViewService' );
+		if ( !$service || !( $service instanceof PageViewService ) ) {
+			$endpoint = $extensionConfig->get( 'PageViewInfoWikimediaEndpoint' );
+			$project = $extensionConfig->get( 'PageViewInfoWikimediaDomain' )
+				?: $mainConfig->get( 'ServerName' );
+
+			$service = new WikimediaPageViewService( $endpoint, [ 'project' => $project ],
+				$extensionConfig->get( 'PageViewInfoWikimediaRequestLimit' ) );
+			$service->setLogger( $logger );
+			$service->setOriginalRequest( RequestContext::getMain()->getRequest() );
+		}
 		$cachedService = new CachedPageViewService( $service, $cache );
 		$cachedService->setCachedDays( $cachedDays );
 		$cachedService->setLogger( $logger );
